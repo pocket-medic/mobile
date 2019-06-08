af43a2108f6d712f553d03d0bab9eabf670aa37b
Surdu <neon-doubt@maskmail.net>
2019-06-08 21:53:21 +0300

    Added log list

--- app/views/diabetesLog/diabetesLog.page.js
+++ app/views/diabetesLog/diabetesLog.page.js
@@ -3,6 +3,14 @@ const utils = require("../../misc/utils");
 
 function onNavigatingTo(args) {
 	viewModel.bindContext(args);
+	viewModel.setData([
+		{ date: "10 Aug", value: 9 },
+		{ date: "12 Aug", value: 12 },
+		{ date: "15 Aug", value: 20 },
+		{ date: "22 Aug", value: 16 },
+		{ date: "23 Aug", value: 14 },
+		{ date: "24 Aug", value: 18 }
+	]);
 }
 
 module.exports = {
--- app/views/diabetesLog/diabetesLog.vm.js
+++ app/views/diabetesLog/diabetesLog.vm.js
@@ -1,21 +1,45 @@
 const BaseViewModel = require("../../misc/BaseViewModel");
 
+const NORM = 15;
 const defaultValues = {
-	norm: [
-		{ date: "22 Aug", value: 15 },
-		{ date: "23 Aug", value: 15 },
-		{ date: "24 Aug", value: 15 }
-	],
-	data: [
-		{ date: "22 Aug", value: 10 },
-		{ date: "23 Aug", value: 20 },
-		{ date: "24 Aug", value: 15 }
-	]
+	norm: null,
+	data: null
 };
 
 class ViewModel extends BaseViewModel {
 	constructor() {
 		super(defaultValues);
+	}
+
+	setData(data) {
+		const norm = [];
+		const formatterData = []
+
+		data.forEach(function(item) {
+			const clone = Object.assign({}, item);
+			clone.value = NORM;
+			norm.push(clone);
+
+			formatterData.push(new LogItem(item));
+		});
+
+		this.set("data", formatterData);
+		this.set("norm", norm);
+	}
+}
+
+class LogItem {
+	constructor(dataItem) {
+		this.date = dataItem.date;
+		this.value = dataItem.value;
+	}
+
+	get classValue() {
+		if (this.value > NORM) {
+			return "list-group-item bad";
+		}
+
+		return "list-group-item";
 	}
 }
 
